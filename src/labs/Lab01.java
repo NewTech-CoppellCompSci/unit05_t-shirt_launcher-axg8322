da7aac11269b2ffc118917c05e3e542e95bfbd53
Aahna Gupta <111918611+axg8322@users.noreply.github.com>
2023-01-15 00:07:37 -0600

    unit 5

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -19,30 +19,64 @@ public class Lab01 {
 	
 	public static void problem1() {
 	
-		
-	}
+		Scanner inKey = new Scanner(System.in);
+		System.out.printf("Enter a positive integer: ");
+		int input1 = inKey.nextInt();
+		System.out.printf("\nEnter another positive integer: ");
+		int input2 = inKey.nextInt();
+		double output = Math.pow(input1, input2);
+		System.out.printf ("\n" +input1 + "^" +input2 +" = ");
+	    System.out.println (output);
+}
 	
 	
 	
 	public static void problem2() {
-		
-			
+	Scanner inKey = new Scanner(System.in);
+	System.out.printf("\nEnter a positive integer: ");
+	int input = inKey.nextInt();
+	double output = (Math.sqrt(input));
+    System.out.printf ("\nThe square root of " +input +" is ");
+    System.out.println (output);					
 	}
 	
 	
 	
 	public static void problem3() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.printf("\nLength of side A: ");
+		int A = inKey.nextInt();
+		System.out.printf("\nLength of side B: ");
+		int B = inKey.nextInt();
+		double H = Math.sqrt (Math.pow (A,2) + Math.pow (B,2));
+	    System.out.println ("\nHypotenuse = " +H );
 	}
 	
 	
 	
 	public static void problem4() {
+		int Max = 0;
+		int Min = 0;
+		System.out.printf("\nEnter an integer: ");
+		Scanner inKey = new Scanner(System.in);
+		int N = inKey.nextInt();
 		
+		while (N != 0)  {
+			if (Max < N) {
+				Max = N;
+				
+			}
+			
+			if (Min > N) {
+				Min = N;
+			}
+			System.out.printf("\nEnter an integer: ");
+			//Scanner inKey = new Scanner(System.in);
+			N = inKey.nextInt();
+			}
+			System.out.println("Max = " +Max);
+			System.out.println("Min = " +Min);
 		
-	}
-	
-	
-	
+			}
 }
+
--- src/performance_task/Performance_Task.java
+++ src/performance_task/Performance_Task.java
@@ -1,15 +1,50 @@
 package performance_task;
 
+import java.util.Scanner;
+
 public class Performance_Task {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+
+
+
 		
-		
-		
-		
-		
-		
-		
+
+System.out.println("Welcome to the Tshirt Launcher! first lets calculate the trajectory of the shirts ");
+System.out.println();
+Scanner inKey = new Scanner (System.in);
+System.out.println("Enter launch velocity (m/s) >>> ");
+double velocity = inKey.nextDouble();
+System.out.println("Enter launch angle (degrees) >>> ");
+double angle = inKey.nextDouble();
+double xpos = 0;
+double ypos = 1;
+int time = 0;
+boolean ground = true;
+System.out.println();
+System.out.println("Projectile's path: \n");
+while (ground) {
+xpos = Math.cos(Math.toRadians(angle)) * time * velocity;
+ypos = Math.sin(Math.toRadians(angle)) * time * velocity - 0.5 * 9.8 * Math.pow(time, 2);
+if (ypos<0) {
+ground = false;
+ypos = 0;
+
+
+				}
+
+System.out.println("Time: " + time + "s\n");
+System.out.println("x-position: " + xpos + "m\n");
+System.out.println("y-position: " + ypos + "m\n");
+
+
+			time++;
+
+
+		}
+
+
 	}
-	
+
 }
+
