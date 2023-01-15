c9010d7cd2b954f1b0b1c8c6085baf2a86ca05fc
Aahna Gupta <111918611+axg8322@users.noreply.github.com>
2023-01-15 00:09:44 -0600

    Update Lab02.java

--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -15,26 +15,66 @@ public class Lab02 {
 
 
 	public static void problem1() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a positive integer: ");
+		int input1 = inKey.nextInt();
+		System.out.print("\nEnter another positive integer: ");
+		int input2 = inKey.nextInt(); 
+		if (input1>0 && input2>0) {
 		
-		
-	}
-
+		int max = Math.max(input1, input2);
+		int min = Math.min(input1, input2);
+		//max = Integer.valueOf (max);
+		//min = Integer.valueOf (min);
+		for (int i=1; i<=100; i++)
+		{
+		int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
+		System.out.printf(random_int +" ");
+		}
+		}
+		}
+	
 
 	public static void problem2() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter the radius: ");
+		double rad = inKey.nextDouble();
+		System.out.print("Enter height: ");
+		double height = inKey.nextDouble();
+		double D = (Math.pow(rad,2)*height)* Math.PI;
+	    System.out.println ("The volume of cylinder is = " +D);
 	}
 
 
 	public static void problem3() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter X1: ");
+		double X1 = inKey.nextDouble();
+		System.out.print("Enter Y1: ");
+		double Y1 = inKey.nextDouble();
+		System.out.print("Enter X2: ");
+		double X2 = inKey.nextDouble();
+		System.out.print("Enter Y2: ");
+		double Y2 = inKey.nextDouble();
+		double D = Math.sqrt((Math.pow ((Y2-Y1),2) + Math.pow ((X2-X1),2))); 
+		System.out.println ("Distance between points = " +D);
 	}
 
 	
 	
 	public static void problem4() {
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("Enter a-value: ");
+		double a = inKey.nextDouble();
+		System.out.print("Enter b-value: ");
+		double b = inKey.nextDouble();
+		System.out.print("Enter c-value: ");
+		double c = inKey.nextDouble();
+		double f1 = Math.sqrt((Math.pow (b,2)-4*a*c));
+	    double X1 = (-b+f1)/(2*a);
+	    double X2 = (-b-f1)/(2*a);
+	    System.out.println ("X1 = " +X1);
+	    System.out.println ("X2 = " +X2);	
 		
 	}
 
